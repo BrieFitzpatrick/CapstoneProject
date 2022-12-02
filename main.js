2bbc0909a0a2628bf32489b54a50391d708052d2
briannafitzpatrick <fitzpatrickb@carleton.edu>
2022-12-02 16:28:01 -0600

    got the button to click

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const newsletter = document.getElementById('newsletter');
+const btn = document.getElementById('submit');
+const form_fields = [fname, lname, email, message, newsletter];
+
+btn.addEventListener("click", function(e){
+  console.log("YOU CLICKED"); //to see if it reconginzes the click and it does!!
+  
+  
+});
